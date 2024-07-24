b8e1862b6d5a607ac82e7674ca3953ac399c8e88
Alexey Inkov <alex0982@mail.ru>
2024-07-25 00:13:51 +0300

    copy model from fastapi

--- django_app/db_device/device/models.py
+++ django_app/db_device/device/models.py
@@ -1,3 +1,96 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class BaseTimeModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Customer(BaseTimeModel):
+    name = models.CharField(max_length=100)
+
+
+    class Meta:
+        verbose_name_plural = "customers"
+
+
+class ServiceOrganization(BaseTimeModel):
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL)
+
+    class Meta:
+        verbose_name_plural = "service_organizations"
+
+
+class MeteringUnit(BaseTimeModel):
+
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL)
+    service_organization = models.ForeignKey(ServiceOrganization, on_delete=models.SET_NULL)
+    address = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = "metering_units"
+
+
+class DeviceRegistryNumber(BaseTimeModel):
+
+    registry_number = models.IntegerField()
+
+    class Meta:
+        verbose_name_plural = "device_registry_numbers"
+
+
+class DeviceType(BaseTimeModel):
+
+    type = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = "device_types"
+
+
+class DeviceMod(BaseTimeModel):
+
+    name = models.CharField(max_length=100)
+    type_devise = models.ForeignKey(DeviceType, on_delete=models.SET_NULL)
+
+    class Meta:
+        verbose_name_plural = "device_mods"
+
+
+class DeviceInstallationPoint(BaseTimeModel):
+
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = "device_installation_points"
+
+
+class Device(BaseTimeModel):
+
+    registry_number = models.ForeignKey(DeviceRegistryNumber, on_delete=models.SET_NULL)
+    device_type = models.ForeignKey(DeviceType, on_delete=models.SET_NULL)
+    device_mod = models.ForeignKey(DeviceMod, on_delete=models.SET_NULL)
+    factory_number = models.CharField(max_length=100)
+    installation_point = models.ForeignKey(
+        DeviceInstallationPoint, on_delete=models.SET_NULL
+    )
+    notes = models.CharField(max_length=100)
+    metering_unit = models.ForeignKey(MeteringUnit, on_delete=models.SET_NULL)
+
+    class Meta:
+        verbose_name_plural = "devices"
+
+
+class DeviceVerification(BaseTimeModel):
+
+    device_id = models.ForeignKey(Device, on_delete=models.SET_NULL)
+    organization = models.CharField(max_length=100)
+    verification_date = models.DateField(default="1900-01-01")
+    valid_date = models.DateField(default="1900-01-01")
+
+    class Meta:
+        verbose_name_plural = "device_verifications"
