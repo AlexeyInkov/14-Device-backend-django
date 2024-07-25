6e129479fc2bb87177611405ff76b1877c33d308
Alexey Inkov <alex0982@mail.ru>
2024-07-26 00:33:31 +0300

    add view organization

--- django_app/db_device/device/views/org.py
+++ django_app/db_device/device/views/org.py
@@ -1,10 +1,20 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpRequest
 from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+    CreateView,
+    UpdateView,
+    DetailView,
+    DeleteView,
+)
 
+from ..models import Organization
 from ..producer import send_message
 
 
 # Create your views here.
-def message_view(request):
+def message_view(request: HttpRequest):
 
     message = "Hello"
     send_message("my_topic", message.encode())
@@ -12,5 +22,29 @@ def message_view(request):
     return render(request, "device/message.html", context=context)
 
 
-class OrganizationView:
-    pass
+class OrganizationListView(LoginRequiredMixin, ListView):
+    model = Organization
+
+    def get_queryset(self):
+        cur_user = self.request.user
+
+        if cur_user.is_staff:
+            return self.model.objects.all()
+        else:
+            return self.model.objects.filter(user=cur_user.pk)
+
+
+class OrganizationCreateView(LoginRequiredMixin, CreateView):
+    model = Organization
+
+
+class OrganizationDetailView(LoginRequiredMixin, DetailView):
+    model = Organization
+
+
+class OrganizationUpdateView(LoginRequiredMixin, UpdateView):
+    model = Organization
+
+
+class OrganizationDeleteView(LoginRequiredMixin, DeleteView):
+    model = Organization
