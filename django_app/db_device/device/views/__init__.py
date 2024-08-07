f2fae0996c315cadc8fe93eeb031bf67e2312585
Alexey Inkov <alex0982@mail.ru>
2024-08-07 23:18:31 +0300

    fix: import view

--- django_app/db_device/device/views/__init__.py
+++ django_app/db_device/device/views/__init__.py
@@ -1,1 +1,15 @@
-from .org import message_view
+from .org import (
+    message_view,
+    OrganizationListView,
+    OrganizationCreateView,
+    OrganizationDetailView,
+    OrganizationUpdateView,
+    OrganizationDeleteView,
+)
+from .metering_unit import (
+    MeteringUnitCreateView,
+    MeteringUnitDetailView,
+    MeteringUnitListView,
+    MeteringUnitUpdateView,
+    MeteringUnitDeleteView,
+)
