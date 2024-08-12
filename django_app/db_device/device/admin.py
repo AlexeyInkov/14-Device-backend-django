f09410e2aee6321b6b102cf1f1bc42a082cde14a
Alexey Inkov <alex0982@mail.ru>
2024-08-12 22:39:19 +0300

    add: admin

--- django_app/db_device/device/admin.py
+++ django_app/db_device/device/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    TSOrganization,
+    Organization,
+    Address,
+    MeteringUnit,
+    Device,
+    DeviceVerification,
+)
+
+admin.site.register(TSOrganization)
+admin.site.register(Organization)
+admin.site.register(Address)
+admin.site.register(MeteringUnit)
+admin.site.register(Device)
+admin.site.register(DeviceVerification)
