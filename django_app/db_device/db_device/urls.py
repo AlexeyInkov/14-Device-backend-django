0c0b657344e22c078d7b90d49e3c31c73e091196
Alexey Inkov <alex0982@mail.ru>
2024-08-13 00:29:55 +0300

    static

--- django_app/db_device/db_device/urls.py
+++ django_app/db_device/db_device/urls.py
@@ -15,10 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("device.urls")),
-]
+from db_device import settings
+
+urlpatterns = (
+    [
+        path("admin/", admin.site.urls),
+        path("", include("device.urls")),
+    ]
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+)
