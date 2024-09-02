7c934c0095e9bd0a796bc4f127034146f33e35c9
Alexey Inkov <alex0982@mail.ru>
2024-09-02 23:30:44 +0300

    add fields into serializers

--- django_app/db_device/device/serializers/address.py
+++ django_app/db_device/device/serializers/address.py
@@ -10,18 +10,25 @@ from ..models import (
 class UserToOrganizationSerializer(MySerializer):
     class Meta:
         model = UserToOrganization
+        fields = "id", "user", "organization"
 
 
 class OrganizationSerializer(MySerializer):
     class Meta:
         model = Organization
+        fields = (
+            "id",
+            "name",
+        )
 
 
 class AddressSerializer(MySerializer):
     class Meta:
         model = Address
+        fields = "__all__"
 
 
 class MeteringUnitSerializer(MySerializer):
     class Meta:
         model = MeteringUnit
+        fields = "__all__"
