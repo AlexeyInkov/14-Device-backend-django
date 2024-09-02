8bad1b0239334985799c4a76edd4982578b751a1
Alexey Inkov <alex0982@mail.ru>
2024-09-02 23:29:42 +0300

    add url /organizations/

--- django_app/db_device/device/urls.py
+++ django_app/db_device/device/urls.py
@@ -1,24 +1,12 @@
-"""
-URL configuration for db_device project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import MenuListAPIView, AddressListAPIView, DeviceListAPIView
+from .views import (
+    MenuListAPIView,
+    AddressListAPIView,
+    DeviceListAPIView,
+    OrganizationListAPIView,
+)
 from .views import TypeToRegistryViewSet, DeviceVerificationViewSet
 
 app_name = "device"
@@ -33,5 +21,6 @@ urlpatterns = [
     path("menu/", MenuListAPIView.as_view(), name="menu"),
     path("addresses/", AddressListAPIView.as_view(), name="addresses"),
     path("devices/", DeviceListAPIView.as_view(), name="devices"),
+    path("organizations/", OrganizationListAPIView.as_view(), name="organizations"),
     path("", include(router.urls)),
 ]
