e7bcfa1488fd3790e4a5b0a80abda7995fad72a1
Alexey Inkov <alex0982@mail.ru>
2024-09-04 00:44:33 +0300

    add urls app account

--- django_app/db_device/db_device/urls.py
+++ django_app/db_device/db_device/urls.py
@@ -1,20 +1,3 @@
-"""
-URL configuration for db_device project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
@@ -41,7 +24,8 @@ urlpatterns = (
             schema_view.with_ui("swagger", cache_timeout=0),
             name="schema-swagger-ui",
         ),
-        path("", include("device.urls")),
+        path("auth/", include("account.urls")),
+        path("api/", include("device.urls")),
     ]
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
