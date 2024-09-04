d71ce1653f4009fbe6f11859d620c1be84dcf2d4
Alexey Inkov <alex0982@mail.ru>
2024-09-05 01:08:59 +0300

    add views

--- django_app/db_device/metering_unit/views.py
+++ django_app/db_device/metering_unit/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.viewsets import ViewSet, ModelViewSet
 
-# Create your views here.
+from metering_unit.mixins import CreateModelViewSetMixin
+from metering_unit.models import Organization, UserToOrganization, Address, MeteringUnit
+from metering_unit.serializers import (
+    OrganizationSerializer,
+    UserToOrganizationSerializer,
+    AddressSerializer,
+    MeteringUnitSerializer,
+)
+
+
+class OrganizationViewSet(CreateModelViewSetMixin, ModelViewSet):
+    queryset = Organization.objects.all()
+    serializer_class = OrganizationSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
+
+
+class UserToOrganizationViewSet(CreateModelViewSetMixin, ModelViewSet):
+    queryset = UserToOrganization.objects.all()
+    serializer_class = UserToOrganizationSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
+
+
+class AddressViewSet(CreateModelViewSetMixin, ModelViewSet):
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
+
+
+class MeteringUnitViewSet(CreateModelViewSetMixin, ModelViewSet):
+    queryset = MeteringUnit.objects.all()
+    serializer_class = MeteringUnitSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
