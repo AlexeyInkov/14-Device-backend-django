e12c45dd7dd63ce779e2cd2c630a380f56169548
Alexey Inkov <alex0982@mail.ru>
2024-09-05 01:09:50 +0300

    add routers

--- django_app/db_device/metering_unit/urls.py
+++ django_app/db_device/metering_unit/urls.py
@@ -1,10 +1,22 @@
 from django.urls import path, include
 from rest_framework import routers
 
+from .views import (
+    MeteringUnitViewSet,
+    AddressViewSet,
+    OrganizationViewSet,
+    UserToOrganizationViewSet,
+)
 
 app_name = "metering_unit"
 
 router = routers.DefaultRouter()
+router.register(r"m_unit", MeteringUnitViewSet, basename="m_unit")
+router.register(r"organization", OrganizationViewSet, basename="organization")
+router.register(
+    r"user_to_organization", UserToOrganizationViewSet, basename="user_to_organization"
+)
+router.register(r"address", AddressViewSet, basename="address")
 
 urlpatterns = [
     path("", include(router.urls)),
