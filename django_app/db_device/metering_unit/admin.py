f547c24227d31027dc2dbe6894bc28044089b40e
Alexey Inkov <alex0982@mail.ru>
2024-09-18 19:44:54 +0300

    admin.py     - Add model Street     - Add model TypeStreet     - Add model Region

--- django_app/db_device/metering_unit/admin.py
+++ django_app/db_device/metering_unit/admin.py
@@ -5,9 +5,15 @@ from .models import (
     Organization,
     Address,
     MeteringUnit,
+    Region,
+    Street,
+    TypeStreet,
 )
 
 admin.site.register(UserToOrganization)
 admin.site.register(Organization)
 admin.site.register(Address)
 admin.site.register(MeteringUnit)
+admin.site.register(Region)
+admin.site.register(TypeStreet)
+admin.site.register(Street)
