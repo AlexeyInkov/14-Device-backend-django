51e366e1634d294c5f43a2fd0d735ce80f1939ae
Alexey Inkov <alex0982@mail.ru>
2024-09-20 00:04:33 +0300

    Add url into metering_init region type_street street

--- django_app/db_device/metering_unit/urls.py
+++ django_app/db_device/metering_unit/urls.py
@@ -2,10 +2,13 @@ from django.urls import path, include
 from rest_framework import routers
 
 from .views import (
-    MeteringUnitViewSet,
-    AddressViewSet,
     OrganizationViewSet,
     UserToOrganizationViewSet,
+    RegionViewSet,
+    TypeStreetViewSet,
+    StreetViewSet,
+    AddressViewSet,
+    MeteringUnitViewSet,
 )
 
 app_name = "metering_unit"
@@ -16,6 +19,9 @@ router.register(r"organization", OrganizationViewSet, basename="organization")
 router.register(
     r"user_to_organization", UserToOrganizationViewSet, basename="user_to_organization"
 )
+router.register(r"region", RegionViewSet, basename="region")
+router.register(r"type_street", TypeStreetViewSet, basename="type_street")
+router.register(r"street", StreetViewSet, basename="street")
 router.register(r"address", AddressViewSet, basename="address")
 
 urlpatterns = [
