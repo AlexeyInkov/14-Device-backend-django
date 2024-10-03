4293a09de4ff8996dfaee776381a8ceb67f23905
Alexey Inkov <alex0982@mail.ru>
2024-10-03 23:13:37 +0300

    add env into constant.js

--- frontend/src/config/constant.js
+++ frontend/src/config/constant.js
@@ -1,12 +1,15 @@
-const debug = true
+// const express = require('express');
+// require('dotenv').config()
+// const app = express();
+export const DEBUG = import.meta.env.VITE_DEBUG==="true" || false;
 export const BASENAME = ''; // не добавляйте '/' в конце BASENAME
-export const BASE_URL = '/app/dashboard/default';
-export const BASE_TITLE = ' | React Datta Able ';
-export const API_SERVER = (!debug ? '/api' : 'http://127.0.0.1:8000');
-console.log(API_SERVER);
-export const URL_MENU = '/v1/page/menu/';
-export const URL_ADDRESSES = '/v1/page/addresses/';
-export const URL_DEVICES = '/v1/page/devices/';
+export const BASE_TITLE = import.meta.env.VITE_VITE_BASE_TITLE;
+export const IP_SERVER = import.meta.env.VITE_IP_SERVER;
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
+export const API_SERVER = (import.meta.env.PROD ? '/api' : 'http://127.0.0.1:8000');
+export const URL_MENU = import.meta.env.VITE_URL_MENU;
+export const URL_ADDRESSES = import.meta.env.VITE_URL_ADDRESSES;
+export const URL_DEVICES = import.meta.env.VITE_URL_DEVICES;
 export const HEADERS_ADDRESS = {
     'id': '№ п/п',
     'customer': 'Абонент',
