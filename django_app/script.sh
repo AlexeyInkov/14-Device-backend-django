509115101953ff21cac9519605970d8f7ccac18d
Alexey Inkov <alex0982@mail.ru>
2024-10-08 00:09:58 +0300

    Merge remote-tracking branch 'origin/deployment' into deployment
    # Conflicts:
    #	django_app/Dockerfile

--- django_app/script.sh
+++ django_app/script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-python manage.py migrate
-python manage.py loaddata fixtures/db_20240918_new_address_region.json
-python manage.py collectstatic
+
+./manage.py migrate
+./manage.py loaddata fixtures/db_20240918_new_address_region.json
+./manage.py collectstatic --noinput
 
